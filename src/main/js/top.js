22df02c7454fc83e077ec3f6d70adf572b4e6454
jun <pickjog@gmail.com>
2024-08-07 21:58:05 +0900

    포트폴리오

---
+++ src/main/js/top.js
@@ -0,0 +1,16 @@
+import "./../css/top.css";
+
+const Top = () => {
+  return (
+    <>
+      <div className="top_container">
+        <p className="top-text" style={{ fontSize: "100px", margin: "0" }}>
+          안녕하세요.<br></br>
+          백엔드 개발자 김준형입니다.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Top;
